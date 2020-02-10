32e15125a148fbd7bedb458deda2c4c547d50bfc
song <songhku925@hotmail.com>
2020-02-09 22:45:04 -0800

    second commit

--- hl-crud.py
+++ hl-crud.py
@@ -47,11 +47,7 @@ class HlCrud:
         return
 
     def delete_data(self, table, where):
-        whr = ''
-        for k, v in where.items():
-            temp = "`" + k + "` = " + "'" + v + "'"
-            whr += " ," + temp if whr else temp
-        sql = "DELETE FROM `%s` WHERE %s " % (table, whr)
+        sql = "DELETE FROM %s WHERE %s " % (table, where)
         x = self.db.cursor()
         x.execute(sql)
         self.db.commit()
@@ -59,7 +55,7 @@ class HlCrud:
 
     def search_data(self, table, where):
         x = self.db.cursor()
-        sql = "SELECT FROM `%s` WHERE %s" % (table, where)
+        sql = "SELECT * FROM %s WHERE %s" % (table, where)
         try:
             x.execute(sql)
             result = x.fetchall()
@@ -71,7 +67,16 @@ class HlCrud:
         except:
             print("wrong condition")
 
+
 if __name__ == '__main__':
     db = HlCrud("config.ini")
-
+    data = {"BoardSN": 1.61, "CompName": "AAA", "Type": "032-1113-000HF", "cModel": "690-6G189-0600-305RH1_BOT",
+            "DLinference_result": "PASS_Hig_0.97",
+            "directory": "TEST_690-6G189-0600-305RH1_1614919000211_AOI_20191217042523", "ConfirmDefect": "PASS",
+            "MachineDefect": "PASS", "Status": "PASS",
+            "image_name": "690-6G189-0600-305RH1_BOT~032-1113-000HF~C1002_1~1614919000211_	SolderLight.jpg_LowAngleLight.jpg_UniformLight.jpg_WhiteLight.jpg",
+            "LightingCondition": "ss", "image_scantime": "33", "BoardRESULT": "sfs"}
     db.show_data("data")
+    db.insert_data("data", data)
+    db.search_data("data", "CompName='AAA'")
+    db.delete_data("data", "CompName='AAA'")
